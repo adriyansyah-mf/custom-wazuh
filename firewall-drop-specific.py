d7c15604d39386e2654548c41dd11d900418b8d3
adriyansyah-mf <adriyansyahmf0@gmail.com>
2024-09-04 14:06:44 +0700

    Push Active Response

---
+++ firewall-drop-specific.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+
+@staticmethod
+def detection_os():
+    """Detection Operation System
+
+    Returns:
+        _str_: Windows|Linux
+    """
+    if os.name == 'nt':
+        return 'Windows'
+    else:
+        return 'Linux'
+
+@staticmethod
+def write_log():
+    """Writing Log Of Active Response
+    """
+
+def block_traffic(ip_address):
+    """Function To Block Traffic
+
+    Args:
+        ip_address (_type_): Attacked IP Address
+        port (_type_): Destination Port
+    """
+
+
+    host_deny_path = '/etc/hosts.deny'
+    try:
+        with open(host_deny_path, "a") as f:
+            f.write(f"sshd: {ip_address}")
+
+        print(f"Blocked {ip_address} to access SSH")
+    except Exception as e:
+        print(f"Unknown Error {str(e)}")
+
+
+if __name__ == '__main__':
+    """
+    Calling Main Function
+    """
+    if len(sys.argv) != 4:
+        print("Usage: Python3 firewall-drop-specific.py ip")
+        sys.exit()
+
+
+    ip_address = sys.argv[1]
+    port = sys.argv[2]
+    type = sys.argv[3]
+
+    block_traffic(ip_address, port, type)
