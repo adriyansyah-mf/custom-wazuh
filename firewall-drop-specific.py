a66e41ed446e66411d32e4516f870432c60b72d4
adriyansyah-mf <adriyansyahmf0@gmail.com>
2024-09-05 13:46:36 +0700

    fix

--- firewall-drop-specific.py
+++ firewall-drop-specific.py
@@ -6,29 +6,25 @@ def block_traffic(ip_address):
     """Function To Block Traffic
 
     Args:
-        ip_address (_type_): Attacked IP Address
-        port (_type_): Destination Port
+        ip_address (str): Attacked IP Address
     """
-
-
     host_deny_path = '/etc/hosts.deny'
     try:
         with open(host_deny_path, "a") as f:
-            f.write(f"sshd: {ip_address}")
+            f.write(f"sshd: {ip_address}\n")  # Add a newline to ensure proper formatting
 
         print(f"Blocked {ip_address} to access SSH")
     except Exception as e:
-        print(f"Unknown Error {str(e)}")
+        print(f"Unknown Error: {str(e)}")
 
 
 if __name__ == '__main__':
     """
-    Calling Main Function
+    Main function to block IP address passed as argument
     """
-    if len(sys.argv) != 4:
-        print("Usage: Python3 firewall-drop-specific.py ip")
+    if len(sys.argv) != 2:
+        print("Usage: python3 firewall-drop-specific.py <ip_address>")
         sys.exit()
-
 
     ip_address = sys.argv[1]
 
